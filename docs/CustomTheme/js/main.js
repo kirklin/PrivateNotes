fda702d12e997ca123af705fc744019694c59903
Kirk Lin <linkirk@163.com>
2022-07-31 14:51:18 +0800

    chore: 定制化主题

--- docs/CustomTheme/js/main.js
+++ docs/CustomTheme/js/main.js
@@ -1,3 +1,15 @@
 (()=>{
-    console.log("Kirk Lin666")
-})()
+    let LabelText = `Kirk Lin`;
+    let LabelStyle = `line-height: 1em;
+            color: #006FEA;
+            display: inline-block;
+            font-size: 11px;
+            line-height: 1em;
+            background-color: #fff;
+            padding: 4px 9px;
+            border-radius: 30px;
+            border: 1px solid rgba(56, 138, 229, 0.16);
+            margin: 4px 5px 4px 0;`;
+    console.log(`%c ${ LabelText }`, LabelStyle,"I'm ready!  ⸜(๑'ᵕ'๑)⸝⋆*");
+    console.log(`%c ${ LabelText }`, LabelStyle,"欢迎查看我的个人笔记");
+})();
